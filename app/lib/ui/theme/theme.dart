a6d4b4f21484399ebf17809bfceedd24cdfccaf5
Mauricio M <mauricio@nolte.io>
2023-06-29 20:52:26 -0300

    Updated pubspec file

--- app/lib/ui/theme/theme.dart
+++ app/lib/ui/theme/theme.dart
@@ -4,65 +4,70 @@ import 'package:google_fonts/google_fonts.dart';
 ThemeData appTheme() {
   return ThemeData(
     textTheme: TextTheme(
-      headline1: GoogleFonts.openSans(
+      displayLarge: GoogleFonts.openSans(
         fontSize: 95,
         fontWeight: FontWeight.w300,
         letterSpacing: -1.5,
       ),
-      headline2: GoogleFonts.openSans(
+      displayMedium: GoogleFonts.openSans(
         fontSize: 59,
         fontWeight: FontWeight.w300,
         letterSpacing: -0.5,
       ),
-      headline3: GoogleFonts.openSans(
+      displaySmall: GoogleFonts.openSans(
         fontSize: 48,
         fontWeight: FontWeight.w400,
       ),
-      headline4: GoogleFonts.openSans(
+      headlineLarge: GoogleFonts.openSans(
+        fontSize: 36,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0.25,
+      ),
+      headlineMedium: GoogleFonts.openSans(
         fontSize: 34,
         fontWeight: FontWeight.w400,
         letterSpacing: 0.25,
       ),
-      headline5: GoogleFonts.openSans(
+      headlineSmall: GoogleFonts.openSans(
         fontSize: 24,
         fontWeight: FontWeight.w400,
       ),
-      headline6: GoogleFonts.openSans(
+      titleLarge: GoogleFonts.openSans(
         fontSize: 20,
         fontWeight: FontWeight.w500,
         letterSpacing: 0.15,
       ),
-      subtitle1: GoogleFonts.openSans(
+      titleMedium: GoogleFonts.openSans(
         fontSize: 16,
         fontWeight: FontWeight.w400,
         letterSpacing: 0.15,
       ),
-      subtitle2: GoogleFonts.openSans(
+      titleSmall: GoogleFonts.openSans(
         fontSize: 14,
         fontWeight: FontWeight.w500,
         letterSpacing: 0.1,
       ),
-      bodyText1: GoogleFonts.roboto(
+      bodyLarge: GoogleFonts.roboto(
         fontSize: 16,
         fontWeight: FontWeight.w400,
         letterSpacing: 0.5,
       ),
-      bodyText2: GoogleFonts.roboto(
+      bodyMedium: GoogleFonts.roboto(
         fontSize: 14,
         fontWeight: FontWeight.w400,
         letterSpacing: 0.25,
       ),
-      button: GoogleFonts.roboto(
+      labelLarge: GoogleFonts.roboto(
         fontSize: 15,
         fontWeight: FontWeight.w600,
         letterSpacing: 1.25,
       ),
-      caption: GoogleFonts.roboto(
+      bodySmall: GoogleFonts.roboto(
         fontSize: 12,
         fontWeight: FontWeight.w400,
         letterSpacing: 0.4,
       ),
-      overline: GoogleFonts.roboto(
+      labelSmall: GoogleFonts.roboto(
         fontSize: 10,
         fontWeight: FontWeight.w400,
         letterSpacing: 1.5,
